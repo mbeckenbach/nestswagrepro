12b7d175e19472bea60b7da55cba22961a8c3c9d
Max Beckenbach <max@bebamail.de>
2021-06-30 09:34:37 +0200

    Add model validation

--- server/main.ts
+++ server/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
   app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT || 4000);
 }
 
