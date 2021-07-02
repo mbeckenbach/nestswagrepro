9cb71d54512fb9e5450614342ed3b1ae8f8a3973
@max126 <max@rocketloop.de>
2021-07-02 09:56:55 +0200

    Add mssql support

--- server/app.module.ts
+++ server/app.module.ts
@@ -5,6 +5,7 @@ import { AppServerModule } from '../src/main.server';
 import { HelloModule } from './modules/hello/hello.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -14,6 +15,24 @@ import { UsersModule } from './modules/users/users.module';
       extraProviders: [
         // Do not remove. Causes that REQUEST is not injected
       ]
+    }),
+    TypeOrmModule.forRoot({
+      // See https://github.com/typeorm/typeorm/blob/master/docs/connection-options.md#mssql-connection-options
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      username: 'sa',
+      password: 'Meister07',
+      database: 'devtest',
+      entities: [],
+      extra: {
+        options: {
+          // TODO: Should be true at azure
+          encrypt: false,
+          instanceName: 'SQLEXPRESS'
+        },
+      },
+      synchronize: true,
     }),
     HelloModule,
     AuthModule,
